7e676f631211ff892e940bfe7e8a9ef335bc3f6a
gnzv1 <gnzl9yt@gmail.com>
2024-03-09 23:55:18 -0300

    OOOOOOOOOOOOOOOOOOK

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,21 +13,21 @@ const Navbar = () => {
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
     {/*logo */}
     <div className="">
-        <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center -z-50">
+        <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center -z-50 relative">
         <span className="text-white mr-1">Test</span>
         <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Hola</span>
         </Link>
     </div>
-    {/* MENU */}
+    {/* MENU RESPONSIVE */}
     <div className="">
       {/* BOTON MENU */}
-      <button className="w-10 h-8 flex flex-col justify-between">
+      <button className="w-10 h-8 flex flex-col justify-between relative">
         <div className="w-10 h-1 bg-white rounded"></div>
-        <div className="w-10 h-1 bg-white rounded"></div>
-        <div className="w-10 h-1 bg-white rounded"></div>
+        <div className="w-10 h-1 bg-black rounded"></div>
+        <div className="w-10 h-1 bg-black rounded"></div>
         </button>
-      {/* LISTA MENU */}
-      <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4x1">
+        {/* LISTA MENU */}
+        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4x1">
         {links.map(link => {
           return (
           <Link href={link.url} key={link.title}>{link.title}</Link>);
